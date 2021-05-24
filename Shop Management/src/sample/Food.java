111dc36ab21b9309783cd40cb5b720b607381eab
Sharif Mohammad Abdullah <64961018+SharifMAbdullah@users.noreply.github.com>
2021-05-24 15:58:46 +0600

    Tried adding counter feature

--- Shop Management/src/sample/Food.java
+++ Shop Management/src/sample/Food.java
@@ -6,6 +6,8 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -13,15 +15,56 @@ import java.io.IOException;
 
 public class Food {
     @FXML
-    private Button btn2;
+    private Button home;
+    @FXML
+    private Button[] btns = new Button[30];
+    @FXML
+    private TextField[] txt = new TextField[30];
+    @FXML
+    private int counts[] = {0};
+    @FXML
+    AnchorPane anchorPane;
+
     public void NewAction2(ActionEvent event) throws IOException {
-        if(event.getSource()==btn2){
+        if (event.getSource() == home) {
             Parent FrontPageController3 = FXMLLoader.load(getClass().getResource("/sample/FrontPageController.fxml"));
             Scene FrontPageScene3 = new Scene(FrontPageController3);
-            Stage FrontPageStage3 = (Stage) ((Node)event.getSource()).getScene().getWindow();
+            Stage FrontPageStage3 = (Stage) ((Node) event.getSource()).getScene().getWindow();
             FrontPageStage3.setTitle("Shop Management");
             FrontPageStage3.setScene(FrontPageScene3);
             FrontPageStage3.show();
+        }
+    }
+
+    /*private Pane getAnchor() {
+        int i = 0;
+        for (Button b : btns) {
+            // do something with your button
+            // maybe add an EventListener or something
+            anchorPane.add(b, i * (i + (int) b.getWidth()), 0);
+            i++;
+        }
+        return anchorPane;
+    }*/
+
+    private void initBtnsArray() {
+        for (int i = 0; i < btns.length; i++) {
+            btns[i] = new Button("Button-" + i);
+        }
+    }
+
+        public void btnACtion(ActionEvent e) {
+        int i = 0;
+        for (Button b : btns) {
+            // do something with your button
+            // maybe add an EventListener or something
+            if (e.getSource() == btns[i])
+            {
+                counts[i]++;
+                String s = counts.toString();
+                txt[i].setText(s);
+            }
+            i++;
         }
     }
 }
